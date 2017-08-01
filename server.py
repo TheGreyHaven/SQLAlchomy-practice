890f305dcab900d4bcced58a75d95cfeb74882b0
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-01 16:31:28 -0700

    WIP registration form

--- server.py
+++ server.py
@@ -2,10 +2,12 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import (Flask, render_template, redirect, request, flash,
+                   session, jsonify)
+
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import connect_to_db, db, User, Rating, Movie
 
 
 app = Flask(__name__)
@@ -22,8 +24,36 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+
+    return render_template("homepage.html")
+
+
+@app.route('/users')
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+
+    return render_template("user_list.html", users=users)
+
+
+@app.route('/register')
+def display_registration_form():
+    """ User registration form. """
+
+    return render_template("register_form.html")
+
+
+@app.route('/register', methods=["POST"])
+def process_registration_form():
+    """ Process registration form. """
+
+    user_email = request.form.get("email")
+    user_password = request.form.get("password")
+
+    
+
+
 
 
 if __name__ == "__main__":
@@ -37,6 +67,4 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     DebugToolbarExtension(app)
 
-
-    
     app.run(port=5000, host='0.0.0.0')
