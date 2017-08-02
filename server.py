4e2d54c88963df9e5dc0dcc13b891afab5b65c0e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-02 13:02:46 -0700

    WIP login form

--- server.py
+++ server.py
@@ -63,6 +63,35 @@ def process_registration_form():
         return redirect('/')
 
 
+@app.route('/login')
+def display_login_form():
+    """ Login form. """
+
+    return render_template("login_form.html")
+
+
+@app.route('/login', methods=["POST"])
+def authenticate_user():
+    """ Checks if email in database. """
+
+    # get info from form, store in var
+    user_email = request.form.get("login-email")
+    user_password = request.form.get("login-password")
+
+    # check if email in DB + password matches (w/ same user_id)
+    if User.query.filter((User.email == user_email) & (User.password == user_password)):
+        # add user to session
+        current_user = User.query.filter((User.email == user_email) & (User.password == user_password))
+        session['current_user'] = current_user.user_id
+        # flash success message + redirect to homepage
+        flash("Logged in as %s" % user_email)
+        return redirect("/")
+        
+            # UPDATE BASE.HTML TO INCLUDE POTENTIAL FLASH MESSAGE
+    # if not (email OR pw wrong), don't let form submit. stay on same page + flash error message.
+
+
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
